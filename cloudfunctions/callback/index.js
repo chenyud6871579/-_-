5cd0657ce664ec497b6f8e2ddd121ac955e04fcd
AA <1277833815@qq.com>
2021-05-17 11:17:38 +0800

    update 客服功能 对特定字段回复
    --user=陈钰鼎 客服功能更新

--- cloudfunctions/callback/index.js
+++ cloudfunctions/callback/index.js
@@ -6,14 +6,21 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  
-  await cloud.openapi.customerServiceMessage.send({
+  if(event.Content =='我发布失败了怎么办'||event.Content =='bug'){await cloud.openapi.customerServiceMessage.send({
+    touser: wxContext.OPENID,
+    msgtype: 'text',
+    text: {
+      content: '你好,这里是失与得小程序客服,您可以刷新或者等待人工客服哦亲',
+    },
+  })}else{
+    await cloud.openapi.customerServiceMessage.send({
     touser: wxContext.OPENID,
     msgtype: 'text',
     text: {
       content: '你好,这里是失与得小程序客服',
     },
-  })
+  })}
+  
 
   return 'success'
 
