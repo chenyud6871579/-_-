d54a531a21e6e38fb2d60904696cca3a78258d12
863320295 <863320295@qq.com>
2021-05-18 18:39:31 +0800

    微调客服回复内容
    --story=1005218 --user=柳鑫

--- cloudfunctions/callback/index.js
+++ cloudfunctions/callback/index.js
@@ -6,22 +6,19 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  if(event.Content =='我发布失败了怎么办'||event.Content =='bug'){await cloud.openapi.customerServiceMessage.send({
+  if(event.Content =='发布'||event.Content =='失败'||event.Content =='bug'){await cloud.openapi.customerServiceMessage.send({
     touser: wxContext.OPENID,
     msgtype: 'text',
     text: {
-      content: '你好,这里是失与得小程序客服,您可以刷新或者等待人工客服哦亲',
+      content: '您好，在发布信息时，若未对照片等个人隐私信息进行模糊处理，则此次发布将会被阻止。在发布过程中，若长时间无响应，您可以尝试刷新或者重新打开微信小程序。',
     },
   })}else{
     await cloud.openapi.customerServiceMessage.send({
     touser: wxContext.OPENID,
     msgtype: 'text',
     text: {
-      content: '你好,这里是失与得小程序客服',
+      content: '您好，这里是失与得小程序客服。',
     },
   })}
-  
-
   return 'success'
-
 }
